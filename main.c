091eca53cc2a76923b0340bea7c01cc62f9c6304
s-full <stefan.fuller1@gmail.com>
2023-04-26 18:38:05 -0700

    finished producer consumer logic, passes testcases

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 typedef int buffer_item;
 #define BUFFER_SIZE 5
@@ -16,6 +17,27 @@ pthread_mutex_t mutex;
 int in = 0;
 int out = 0;
 
+int running = 1;
+
+void print_buffer() {
+
+    // prints buffer, prints empty if value is -1
+    
+    printf("\t[");
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+        if (buffer[i] == EMPTY) {
+            printf("empty");
+        } else {
+            printf("%d", buffer[i]);
+        }
+        if (i < BUFFER_SIZE - 1) {
+            printf("][");
+        }
+    }
+    printf("] in = %d, out = %d\n", in, out);
+
+}
+
 int insert_item(int item) 
 {
     sem_wait(&empty);
@@ -23,8 +45,13 @@ int insert_item(int item)
 
     // critical section
 
+    
     buffer[in] = item;
+   
+
+    printf("insert_item inserted item %d at position %d", item, in);
     in = (in + 1) % BUFFER_SIZE;
+    print_buffer();
 
     // end critical section
 
@@ -41,13 +68,20 @@ int remove_item()
 
     // critical section
 
+    if (buffer[out] == EMPTY) {
+        return -1;
+    }
+
     buffer_item item = buffer[out];
     buffer[out] = EMPTY;
     out = (out+1) % BUFFER_SIZE;
 
+    printf("remove_item removed item %d at position %d", item, out);
+    print_buffer();
+
     // end critical section
 
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_unlock(&mutex);
     sem_post(&empty);
 
     return item;
@@ -57,43 +91,54 @@ int remove_item()
 
 void *producer(void *arg)
 {
+    pthread_t tid = pthread_self();
+    printf("Creating producer thread with id %lu\n", tid);
     buffer_item item;
-    while (true)
+    while (running)
     {
         srand(time(NULL));
         int sleepy_time = rand() % 3 +1;
+        printf("Producer thread %lu sleeping for %d seconds\n ", tid, sleepy_time);
         sleep(sleepy_time);
 
         item = rand()%100+1;
-        printf("producer produced %d\n", item);
-        if (insert_item(item) < 0) {
-            printf("Producer error\n");
-        }   
+        printf("Producer thread %lu inserted %d\n", tid, item);
+        insert_item(item);   
     }
+
+    pthread_exit(NULL);
 }
 
 void *consumer(void *arg)
 {
-    
+    pthread_t tid = pthread_self();
+    printf("Creating consumer thread with id %lu\n", tid);
     buffer_item item;
-    while (true)
+    while (running)
      {
           srand(time(NULL));
           int sleepy_time = rand() % 3 +1;
+          printf("Consumer thread %lu sleeping for %d seconds\n", tid, sleepy_time);
           sleep(sleepy_time);
 
-         item = remove_item();  
+          item = remove_item();  
     }
+
+    pthread_exit(NULL);
     
 }
 
-main(int argc, char *argv[]) 
+int main(int argc, char *argv[]) 
 {
     in = 0;
     out = 0;
     pthread_mutex_init(&mutex, NULL);
     sem_init(&empty, 0, BUFFER_SIZE);
     sem_init(&full, 0, 0);
+
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+        buffer[i] = EMPTY;
+    }
 
 /* 1. Get command line arguments */
 /* 2. Initialize buffer */
@@ -102,26 +147,29 @@ main(int argc, char *argv[])
 /* 5. Sleep. */
 /* 6. Main thread exits. */
 
-    if (argc != 3) {
-        printf("Invalid args\n");
-        exit(-1);
-    }
+    // if (argc != 4) {
+    //     printf("Invalid args\n");
+    //     exit(-1);
+    // }
 
-    // get cmd line args
-    int time = argv[1];
-    int num_prods = argv[2];
-    int num_consums = argv[3];
+    //get cmd line args
+    int time = atoi(argv[1]);
+    int num_prods = atoi(argv[2]);
+    int num_consums = atoi(argv[3]);
 
     // alloc threads
-    pthread_t *consumer_threads = malloc(sizeof(pthread_t) * num_consums);
-    pthread_t *producer_threads = malloc(sizeof(pthread_t) * num_prods);
+    pthread_t *consumer_threads;// = malloc(sizeof(pthread_t) * num_consums);
+    pthread_t *producer_threads;// = malloc(sizeof(pthread_t) * num_prods);
+
+    consumer_threads = (pthread_t *) malloc(num_consums * sizeof(pthread_t));
+    producer_threads = (pthread_t *) malloc(num_prods * sizeof(pthread_t));
 
     // check if threads not allocated
-    if (consumer_threads == NULL || producer_threads == NULL)
-    {
-        printf("ERROR: failed to create threads");
-        exit(1);
-    }
+    // if (consumer_threads == NULL || producer_threads == NULL)
+    // {
+    //     printf("ERROR: failed to create threads");
+    //     exit(1);
+    // }
 
     // create prod threads
    
@@ -131,34 +179,37 @@ main(int argc, char *argv[])
             printf("ERROR: Failed in creating producer thread %d", i);
             exit(2);
         }
+        
     }
     // create consum threads
     for (int i = 0; i < num_prods; i++) {
-        int rc = pthread_create(&consumer_threads[i],NULL, consumer, NULL);
+        unsigned long rc = pthread_create(&consumer_threads[i],NULL, consumer, NULL);
         if (rc) {
             printf("ERROR: Failed in creating consumer thread %d", i);
             exit(3);
         }
+        
     }
 
+    printf("Main thread sleep for %d seconds", time);
     sleep(time);
+    running = 0;
 
-    // join producer threads
-    for (int i = 0; i < num_prods; i++) {
-        int rc = pthread_join(&producer_threads[i], NULL);
-        if (rc) {
-            printf("ERROR: Failed in joining producer thread %d", i);
-            exit(4);
-        }
-    }
+    printf("Main thread exiting\n");
 
-    // join consumer threads
-    for (int i = 0; i < num_consums; i++) {
-        int rc = pthread_join(&consumer_threads[i], NULL);
-        if (rc) {
-            printf("ERROR: Failed in creating producer thread %d", i);
-            exit(5);
-        }
-    }
-    return 0;
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
+
+    // //join producer threads
+    //  for (int i = 0; i < num_prods; i++) {
+    //     pthread_join(producer_threads[i], NULL);
+       
+    // }
+
+    //  // join consumer threads
+    // for (int i = 0; i < num_consums; i++) {
+    //     pthread_join(consumer_threads[i], NULL);
+    // }
+    exit(0);
 }
